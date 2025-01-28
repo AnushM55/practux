cd4996ae882f5a83182907707652b0ec5a980e32
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2025-01-28 07:10:20 +0530

    misc changes

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,6 +1,6 @@
 import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
-import ContestMenus from "@/components/events";
 import { createClient } from "@/utils/supabase/server";
+
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
